107e3730599608d1b199e3995191c1d3e5bd7c90
enjee <ngiarrusso@gmail.com>
2019-04-11 17:19:01 -0400

    Test all methods successful

--- lib/game_turn.rb
+++ lib/game_turn.rb
@@ -7,4 +7,28 @@ class GameTurn
     @row_index = row_index
     @col_index = col_index
   end
+
+  def take!
+    @board.rows[row_index][@col_index].player = @player
+  end
+
+  def winner?
+    horizontal_win? || vertical_win?
+  end
+
+  def horizontal_win?
+    victory = true
+    board.rows[row_index].each do |space|
+      victory = false if space.player != @player
+    end
+    victory
+  end
+
+  def vertical_win?
+    victory = true
+    board.rows.each do |row|
+      victory = false if row[@col_index].player != @player
+    end
+    victory
+  end
 end
--- spec/game_turn_spec.rb
+++ spec/game_turn_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe GameTurn do
   let(:board) { Board.new}
   let(:player) { Player.new('x') }
+  let(:opposing_player) { Player.new('o') }
   let(:row_index) { 0 }
   let(:col_index) { 0 }
   let(:turn) { GameTurn.new(board, player, row_index, col_index)}
@@ -25,13 +26,62 @@ describe GameTurn do
 
   context "checking for win conditions" do
     context "horizontally" do
-      it "does have a win if a row of the same player is found"
-      it "does not have a win if a row is interrupted by another player"
-      it "does not have a win if an unoccupied sapce is found in the row"
+      it "does have a win if a row of the same player is found" do
+        winning_board = Board.new
+        2.times do |index|
+          winning_board.add_turn(player, 0, index)
+        end
+
+        winning_turn = GameTurn.new(winning_board, player, 0, 2)
+        winning_turn.take!
+        expect(winning_turn).to be_winner
+      end
+
+      it "does not have a win if a row is interrupted by another player" do
+        losing_board = Board.new
+        losing_board.add_turn(opposing_player, 0, 0)
+        losing_board.add_turn(opposing_player, 0, 1)
+
+        losing_turn = GameTurn.new(losing_board, player, 0, 2)
+        losing_turn.take!
+        expect(losing_turn).to_not be_winner
+      end
+
+      it "does not have a win if an unoccupied space is found in the row" do
+        losing_board = Board.new
+        losing_turn = GameTurn.new(losing_board, player, 0, 0)
+        expect(losing_turn).to_not be_winner
+      end
     end
 
     context "vertically" do
+      it "is a winner if the column is occupied by one player exclusively" do
+        winning_board = Board.new
+        2.times do |index|
+          winning_board.add_turn(player, index, 0)
+        end
 
+        winning_turn = GameTurn.new(winning_board, player, 2, 0)
+        winning_turn.take!
+        expect(winning_turn).to be_winner
+      end
+
+      it "is not a winner if the column is interrupted by another player" do
+        losing_board = Board.new
+        losing_board.add_turn(opposing_player, 0, 0)
+        losing_board.add_turn(opposing_player, 1, 0)
+
+
+        losing_turn = GameTurn.new(losing_board, player, 2, 0)
+        losing_turn.take!
+        expect(losing_turn).to_not be_winner
+      end
+
+      it "is not a winner if an unoccupied space is found in the column" do
+        losing_board = Board.new
+        losing_turn = GameTurn.new(losing_board, player, 0, 0)
+        expect(losing_turn).to_not be_winner
+      end
     end
   end
 end
