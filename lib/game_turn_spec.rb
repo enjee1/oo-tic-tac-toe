ffa2ae65818599234d242d1e16d1acb99ce997f9
enjee <ngiarrusso@gmail.com>
2019-04-11 12:50:44 -0400

    Fourth iteration

--- lib/game_turn_spec.rb
+++ lib/game_turn_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe GameTurn do
   let(:board) { Board.new}
   let(:player) { Player.new('x') }
+  let(:row_index) { 0 }
+  let(:col_index) { 0 }
 
   it "has a board" do
     turn = GameTurn.new(board, player)
@@ -15,10 +17,14 @@ describe GameTurn do
   end
 
   it "has a row index" do
-    GameTurn.new(board, player, 0)
+    GameTurn.new(board, player, row_index)
+    expect(turn.row_index).to eq(row_index)
   end
 
-  it "has a column index"
+  it "has a column index" do
+    GameTurn.new(board, player, col_index)
+    expect(turn.col_index).to eq(col_index)
+  end
 
   context "checking for win conditions" do
     context "horizontally" do
